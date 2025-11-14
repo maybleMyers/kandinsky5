e1aacc0ebe50bfa1c0d179a3039530bcf2fafa0c
maybleMyers <benjimon678@yahoo.com>
2025-11-14 12:44:15 -0800

    modify port

--- k1.py
+++ k1.py
@@ -276,4 +276,4 @@ def create_interface():
 
 if __name__ == "__main__":
     demo = create_interface()
-    demo.launch(server_name="0.0.0.0", server_port=7860, share=False)
+    demo.launch(server_name="0.0.0.0", share=False)
